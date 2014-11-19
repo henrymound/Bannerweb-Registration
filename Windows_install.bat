efdd4195b17408c520964b8a7e109ce4b8735764
Button <jbutton@middlebury.edu>
2014-11-19 10:57:08 -0500

    Simple Install procedure for Windows
    Created batch file for install, also runs the program following initial
    installation

---
+++ Windows_install.bat
@@ -0,0 +1,18 @@
+@echo off
+set curpath=%~d0%~p0
+
+echo %curpath%
+
+pushd %curpath%\Dist
+
+@ msiexec.exe /i .\python-2.7.8.msi /QN /L*V "C:\Temp\msilog.log"
+Start python-2.7.8.msi
+echo Press enter when done installing python
+pause
+
+C:\Python27\python.exe ez_install_setup.py
+
+C:\Python27\Scripts\easy_install.exe mechanize
+C:\Python27\python Midd_Script.py
+
+popd 
